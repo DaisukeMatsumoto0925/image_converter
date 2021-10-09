f4207da9ac8fb2ddb358f60fe86a34b917e6ca3a
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-10-09 14:21:43 +0900

    imple main

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/DaisukeMatsumoto0925/image_converter/convert"
+)
+
+var (
+	srcDir = flag.String("src", ".", "変換前画像があるディレクトリ(相対パス)")
+	dstDir = flag.String("dst", "./result", "変換後画像を配置するディレクトリ")
+	bExt   = flag.String("b", "jpg", "変換前の画像拡張子")
+	aExt   = flag.String("a", "png", "変換後の画像拡張子")
+)
+
+func main() {
+	flag.Parse()
+
+	cvt, err := convert.NewConverter(*srcDir, *dstDir, *bExt, *aExt)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	err = cvt.Do()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+}
