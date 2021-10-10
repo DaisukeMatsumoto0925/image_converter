d97ab062a18266bbaad32b2f7b993418cbff05d0
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-10-10 21:10:12 +0900

    add tests

--- convert/converter_test.go
+++ convert/converter_test.go
@@ -54,3 +54,104 @@ func TestNewConverter(t *testing.T) {
 		})
 	}
 }
+
+func TestConverter_convert(t *testing.T) {
+	type fields struct {
+		srcDirPath string
+		dstDirPath string
+		bExt       string
+		aExt       string
+	}
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Converter{
+				srcDirPath: tt.fields.srcDirPath,
+				dstDirPath: tt.fields.dstDirPath,
+				bExt:       tt.fields.bExt,
+				aExt:       tt.fields.aExt,
+			}
+			if err := c.convert(tt.args.path); (err != nil) != tt.wantErr {
+				t.Errorf("Converter.convert() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestConverter_getOutputFileName(t *testing.T) {
+	type fields struct {
+		srcDirPath string
+		dstDirPath string
+		bExt       string
+		aExt       string
+	}
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Converter{
+				srcDirPath: tt.fields.srcDirPath,
+				dstDirPath: tt.fields.dstDirPath,
+				bExt:       tt.fields.bExt,
+				aExt:       tt.fields.aExt,
+			}
+			got, err := c.getOutputFileName(tt.args.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Converter.getOutputFileName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Converter.getOutputFileName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConverter_Do(t *testing.T) {
+	type fields struct {
+		srcDirPath string
+		dstDirPath string
+		bExt       string
+		aExt       string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Converter{
+				srcDirPath: tt.fields.srcDirPath,
+				dstDirPath: tt.fields.dstDirPath,
+				bExt:       tt.fields.bExt,
+				aExt:       tt.fields.aExt,
+			}
+			if err := c.Do(); (err != nil) != tt.wantErr {
+				t.Errorf("Converter.Do() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
